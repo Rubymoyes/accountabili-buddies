ed7f48cb8eac3ede964c81801c1dcbc36e83ff6e
Arry <Arry@Arry.arr>
2018-11-08 20:30:00 +1300

    forms

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,19 @@
-<html>
-<body>
 
-<h2>Log in</h2>
+//used the template from taz and i's taxonomic nav page
+import React from "react";
+import { Link } from "react-router-dom"; //obviously shouldnt be link, not sure what else to put in there
 
-/ not sure what the action section relates to
+const forms = () => {
+  
+  
+  return (
+    // below this have just put in a basic form for log in and sign up, have not tailored it to react
+    //not sure what the action tag needs to be filled with so have left it with the generic one from w3 schools
+    <div>
+      <h2>Log in</h2>
+
+      
+
 <form action="/action_page.php" method="get">
   <fieldset>
     <legend>Log in:</legend>
@@ -17,7 +27,43 @@
   </fieldset>
 </form>
 
-</body>
-</html>
+
+<h2>Sign up</h2>
+
+/ not sure what the action section relates to
+<form action="/action_page.php" method="post">
+  <fieldset>
+    <legend>Sign Up:</legend>
+    Email address:<br>
+    <input type="text" name="Email" value="">
+    <br>
+    Password:<br>
+    <input type="password" name="Password" value="">
+    <br><br>
+    Repeat Password:<br>
+    <input type="password" name="Password" value="">
+    <br><br>
+    <input type="submit" value="Submit">
+  </fieldset>
+</form>
+    </div>
+  );
+};
+
+export default forms;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
