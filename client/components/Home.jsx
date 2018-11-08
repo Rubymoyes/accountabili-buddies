6d44d37def02d1c4404e523ee99df6c1d8aa087a
Arry <Arry@Arry.arr>
2018-11-08 20:16:07 +1300

    forms

---
+++ client/components/Home.jsx
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<h2>Log in</h2>
+
+/ not sure what the action section relates to
+<form action="/action_page.php" method="get">
+  <fieldset>
+    <legend>Log in:</legend>
+    Email address:<br>
+    <input type="text" name="Email" value="">
+    <br>
+    Password:<br>
+    <input type="password" name="Password" value="">
+    <br><br>
+    <input type="submit" value="Submit">
+  </fieldset>
+</form>
+
+</body>
+</html>
+
+
