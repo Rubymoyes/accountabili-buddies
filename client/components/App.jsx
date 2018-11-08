552d108670fc0e411a1f8daf5b4a9a1ed87e2c8c
Arry <Arry@Arry.arr>
2018-11-09 09:29:30 +1300

    updating branch

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from "react";
+import Home from "./Home";
 
 const App = () => {
   return (
-    <h1>
-      Welcome to Accountabilibuddies - The 1# Survey and Forum Website!
-      </h1>
-  )
-}
+    <h1>Welcome to Accountabilibuddies - The 1# Survey and Forum Website!</h1>
+  );
+};
 
-export default App
-
+export default App;
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,56 +1,57 @@
 
 //used the template from taz and i's taxonomic nav page
 import React from "react";
-import { Link } from "react-router-dom"; //obviously shouldnt be link, not sure what else to put in there
+import { HashRouter as Router, Route} from "react-router-dom"; //obviously shouldnt be link, not sure what else to put in there
+
 
 const forms = () => {
   
   
   return (
-    // below this have just put in a basic form for log in and sign up, have not tailored it to react
-    //not sure what the action tag needs to be filled with so have left it with the generic one from w3 schools
-    <div>
-      <h2>Log in</h2>
+//     // below this have just put in a basic form for log in and sign up, have not tailored it to react
+//     //not sure what the action tag needs to be filled with so have left it with the generic one from w3 schools
+//     <div>
+//       <h2>Log in</h2>
 
       
 
-<form action="/action_page.php" method="get">
-  <fieldset>
-    <legend>Log in:</legend>
-    Email address:<br>
-    <input type="text" name="Email" value="">
-    <br>
-    Password:<br>
-    <input type="password" name="Password" value="">
-    <br><br>
-    <input type="submit" value="Submit">
-  </fieldset>
-</form>
+// <form action="/action_page.php" method="get">
+//   <fieldset>
+//     <legend>Log in:</legend>
+//     Email address:<br>
+//     <input type="text" name="Email" value="">
+//     <br>
+//     Password:<br>
+//     <input type="password" name="Password" value="">
+//     <br><br>
+//     <input type="submit" value="Submit">
+//   </fieldset>
+// </form>
 
 
-<h2>Sign up</h2>
+// <h2>Sign up</h2>
 
-/ not sure what the action section relates to
-<form action="/action_page.php" method="post">
-  <fieldset>
-    <legend>Sign Up:</legend>
-    Email address:<br>
-    <input type="text" name="Email" value="">
-    <br>
-    Password:<br>
-    <input type="password" name="Password" value="">
-    <br><br>
-    Repeat Password:<br>
-    <input type="password" name="Password" value="">
-    <br><br>
-    <input type="submit" value="Submit">
-  </fieldset>
-</form>
-    </div>
-  );
-};
+// / not sure what the action section relates to
+// <form action="/action_page.php" method="post">
+//   <fieldset>
+//     <legend>Sign Up:</legend>
+//     Email address:<br>
+//     <input type="text" name="Email" value="">
+//     <br>
+//     Password:<br>
+//     <input type="password" name="Password" value="">
+//     <br><br>
+//     Repeat Password:<br>
+//     <input type="password" name="Password" value="">
+//     <br><br>
+//     <input type="submit" value="Submit">
+//   </fieldset>
+// </form>
+//     </div>
+//   );
+// };
 
-export default forms;
+ export default Home;
 
 
 
