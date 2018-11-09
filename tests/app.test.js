0fad9e1b4b075f43bc95459d0b41a9a696228a9c
Arry <Arry@Arry.arr>
2018-11-09 14:44:32 +1300

    checking out brandon, going to test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import {shallow} from 'enzyme'
+import React from "react";
+import { shallow } from "enzyme";
 
-import App from '../client/components/App'
+import App from "../client/components/App";
 
-test('<App />', () => {
-  const expected = 'React development has begun!'
-  const wrapper = shallow(<App />)
-  expect(wrapper.text()).toBe(expected)
-})
+test("<App />", () => {
+  const expected = "React development has begun!";
+  const wrapper = shallow(<App />);
+  expect(wrapper.text()).toBe(expected);
+});
+
+test("<App />", () => {
+  const expected =
+    "Welcome to Accountabilibuddies - The 1# Survey and Forum Website!";
+  const wrapper = shallow(<App />);
+  expect(wrapper.text()).toBe(expected);
+});
